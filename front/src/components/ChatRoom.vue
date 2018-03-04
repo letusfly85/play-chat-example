e136c5c0cf4dd038225f53c846c57d8818d0d867
letusfly85 <letusfly95@gmail.com>
2018-03-04 12:49:41 +0900

    add card to display chat message

--- front/src/components/ChatRoom.vue
+++ front/src/components/ChatRoom.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <MdButton class="md-button">Enter Chat Room</MdButton>
+    <div v-for="message in messageQueue" v-bind:key="message.id">
+      <md-card>
+        <md-card-content>
+          {{ message }}
+        </md-card-content>
+      </md-card>
+    </div>
   </div>
 </template>
 
@@ -9,15 +16,29 @@
 
 export default {
   name: 'ChatRoom',
+  data () {
+    return {
+      messageQueue: []
+    }
+  },
   mounted () {
     let wsuri = 'ws://localhost:9000/chat/myroom?user-id=14'
     const ws = new WebSocket(wsuri)
 
+    const self = this
     ws.onopen = function () {
       ws.send(JSON.stringify({message: 'test'}))
     }
+    var i = 0
     ws.onmessage = function (message) {
       console.log(message)
+      let json = JSON.parse(message.data)
+      if (json.request_message !== undefined) {
+        console.log(json)
+        self.message = json.request_message.message
+        self.messageQueue.push({id: i, message: self.message})
+        i = i + 1
+      }
     }
     ws.onclose = function () {
       console.log('closed')
