d9cad9a01a4da0198d3b8d034b6b41856c0dab60
letusfly85 <letusfly95@gmail.com>
2018-03-04 23:25:30 +0900

    add button

--- front/src/components/ChatRoom.vue
+++ front/src/components/ChatRoom.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <MdButton class="md-button">Enter Chat Room</MdButton>
+    <md-button class="md-button" @click="enterRoom('myroom')">Enter Chat Room</md-button>
     <div v-for="message in messageQueue" v-bind:key="message.id">
       <md-card>
         <md-card-content>
@@ -8,6 +8,12 @@
         </md-card-content>
       </md-card>
     </div>
+    <form @submit="sendMessage">
+      <md-field md-inline>
+        <md-input v-model="form.message"></md-input>
+      </md-field>
+      <md-button type="submit" submit="sendMessage">Send Message</md-button>
+    </form>
   </div>
 </template>
 
@@ -18,32 +24,51 @@ export default {
   name: 'ChatRoom',
   data () {
     return {
-      messageQueue: []
+      messageId: 0,
+      ws: null,
+      messageQueue: [],
+      form: {
+        message: null
+      }
+    }
+  },
+  methods: {
+    enterRoom: function (roomName) {
+      let wsuri = 'ws://localhost:9000/chat/' + roomName + '?user-id=14'
+      this.ws = new WebSocket(wsuri)
+    },
+    sendMessage: function () {
+      console.log(this.form.message)
+      this.ws.send(JSON.stringify({message: 'test'}))
+      this.messageQueue.push({id: this.messageId, message: this.form.message, from: 'To'})
+      this.messageId = this.messageId + 1
+      this.form.message = null
     }
   },
   mounted () {
     let wsuri = 'ws://localhost:9000/chat/myroom?user-id=14'
-    const ws = new WebSocket(wsuri)
+    this.ws = new WebSocket(wsuri)
 
     const self = this
-    ws.onopen = function () {
-      ws.send(JSON.stringify({message: 'test'}))
+    this.ws.onopen = function () {
+      if (this.ws !== undefined && this.ws !== null) {
+        this.ws.send(JSON.stringify({message: 'test'}))
+      }
     }
-    var i = 0
-    ws.onmessage = function (message) {
+    this.ws.onmessage = function (message) {
       console.log(message)
       let json = JSON.parse(message.data)
       if (json.request_message !== undefined) {
         console.log(json)
         self.message = json.request_message.message
-        self.messageQueue.push({id: i, message: self.message})
-        i = i + 1
+        self.messageQueue.push({id: self.messageId, message: self.message, from: 'From'})
+        self.messageId = self.messageId + 1
       }
     }
-    ws.onclose = function () {
+    this.ws.onclose = function () {
       console.log('closed')
     }
-    ws.onerror = function (error) {
+    this.ws.onerror = function (error) {
       console.log(error)
     }
   }
